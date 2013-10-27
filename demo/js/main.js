48cddaae9ead73ea49d1a6cfcebd0b4782402e62
Eman Calso <egcalso@gmail.com>
2013-10-28 07:42:19 +0800

    Save the values to a test location

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,7 +1,78 @@
-var $bledge = {};
+var bl = {};
+var aoeu;
 
 $(function(){
-	console.log('Hello, world!');
-	$bledge.root = {};
-	$bledge.ref = new Firebase('https://bledger.firebaseIO.com/');
+	var $l = $('#bledger');
+	var $lin = $('#bl-in');
+	var $lout = $('#bl-out');
+	bl.root = {};
+	bl.ref = new Firebase('https://bledger.firebaseIO.com/');
+	bl.vals = [];
+
+	bl.add = function(direction, amount, tags, description) {
+		var entry = {
+			direction: direction,
+			amount: amount,
+			tags: tags,
+			description: description,
+		};
+		bl.vals.push(entry);
+		bl.test.set(bl.vals);
+	}
+
+	bl.flush = function() {
+		bl.test.set([])
+	}
+
+	bl.display = function (i, e) {
+		var tags = '';
+		if (e.tags) tags = e.tags;
+		$row = $('<div class="row">' +
+			'<div class="col-md-4">' + e.amount + '</div>' +
+			'<div class="col-md-4">Tags: ' + tags + '</div>' +
+			'<div class="col-md-4">Desc: ' + e.description + '</div>' +
+			'</div>');
+		if (e.direction == 'in') {
+			$lin.prepend($row);
+		} else {
+			$lout.prepend($row);
+		}
+	}
+
+	bl.test = new Firebase('https://bledger.firebaseIO.com/test/');
+	bl.test.on('value', function(snap) {
+		bl.vals = snap.val();
+		if (bl.vals == null) {
+			bl.vals = [];
+			bl.test.set([]);
+		}
+		console.log('Values ' + snap.name() + ': ' + bl.vals);
+		bl.test.off('value');
+	});
+	bl.test.on('child_added', function(snap) {
+		val = snap.val();
+		if (val == null) {
+			bl.vals = [];
+			bl.test.set([]);
+		}
+		console.log('Values ' + snap.name() + ': ' + val);
+		bl.display(0, val);
+	});
+
+	$('#form-in').submit(function(e) {
+		var vals = {direction: 'in', amount: 0, tags: [], description: ''};
+		$.each($(this).serializeArray(),
+			function(i, e) {vals[e.name] = e.value;});
+		bl.add(vals.direction, vals.amount, vals.tags, vals.description);
+		return false;
+	});
+
+	$('#form-out').submit(function(e) {
+		var vals = {direction: 'out', amount: 0, tags: [], description: ''};
+		$.each($(this).serializeArray(),
+			function(i, e) {vals[e.name] = e.value;});
+		bl.add(vals.direction, vals.amount, vals.tags, vals.description);
+		return false;
+	});
+
 });
